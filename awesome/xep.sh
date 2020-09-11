eaa13a0bbef763e5add8626369705a73c57e1ff1
Simon Bundgaard-Egeberg <simon@bundgaard-egeberg.dk>
2020-09-11 11:03:14 +0200

    using stow

--- awesome/xep.sh
+++
@@ -1,4 +0,0 @@
-Xephyr :1 -screen 1280x800 &
-DISPLAY=:1 $HOME/awesome/bin/awesome \
-    -c $HOME/.config/awesome/rc.lua \
-    --search $HOME/awesome/share/awesome/lib
--- zsh/prompt.zsh
+++
@@ -1,2 +0,0 @@
-zinit wait'!' lucid atload'source ~/.p10k.zsh; _p9k_precmd' nocd light-mode for \
-  romkatv/powerlevel10k
