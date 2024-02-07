7f59e721998add7a3f8e13e15f20aa04b730e535
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2024-02-07 11:17:53 +0100

    Automated commit: Wed Feb  7 11:17:53 AM CET 2024

--- configs/.config/doom/config.el
+++ configs/.config/doom/config.el
@@ -40,8 +40,9 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
+;;
 (setq org-directory "~/org/")
-
+(setq doom-font "0xProto Nerd Font")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
