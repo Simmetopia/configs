c2956c27c2be388751ef84568b69d2252ea4f5bf
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2024-02-07 11:43:52 +0100

    Automated commit: Wed Feb  7 11:43:52 AM CET 2024

--- configs/.config/doom/flycheck_config.el
+++ configs/.config/doom/flycheck_config.el
@@ -49,9 +49,8 @@
 (setq doom-font "0xProto Nerd Font")
 
 (after! 'elixir (
-                 lsp-elixir-local-server-command /home/simmetopia/
-                 ))
-;; Whenever you reconfigure a package, make sure to wrap your config in an
+                 lsp-elixir-local-server-command you reconfigure a package, make sure to wrap your config in an
+                                                 
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
 ;;   (after! PACKAGE
