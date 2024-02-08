1f935cfab3b1f5b44dc848f7e58d85e9f5f61bdd
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2024-02-08 12:37:08 +0100

    Automated commit: Thu Feb  8 12:37:08 PM CET 2024

--- configs/.config/nvim/lua/plugins/nvim_tree.lua
+++ configs/.config/nvim/lua/plugins/nvim_tree.lua
@@ -6,7 +6,7 @@ local M = {
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   }
@@ -15,16 +15,16 @@ local M = {
 M.config = function()
   require("neo-tree").setup({
     filesystem = {
-      bind_to_cwd = true,    -- true creates a 2-way binding between vim's cwd and neo-tree's root
+      bind_to_cwd = true,  -- true creates a 2-way binding between vim's cwd and neo-tree's root
       cwd_target = {
-        sidebar = "tab",     -- sidebar is when position = left or right
-        current = "window"   -- current is when position = current
+        sidebar = "tab",   -- sidebar is when position = left or right
+        current = "window" -- current is when position = current
       },
-    },
-    follow_current_file = {
-      enabled = true,                -- This will find and focus the file in the active buffer every time
-      --              -- the current file is changed while the tree is open.
-      leave_dirs_open = false,       -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      follow_current_file = {
+        enabled = true,        -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
     },
   })
 end
