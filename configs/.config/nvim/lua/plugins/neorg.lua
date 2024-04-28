929e302c0695a4d7095e16c5546ccbe301522079
Simon Bundgaard-Egeberg <simon@bundgaard-egeberg.dk>
2024-04-28 15:42:34 +0200

    sync

--- configs/.config/nvim/lua/plugins/neorg.lua
+++ configs/.config/nvim/lua/plugins/neorg.lua
@@ -11,6 +11,51 @@ local M = {
   version = "*", -- Pin Neorg to the latest stable release
 }
 
+local function updateIndexAndRunNeorg()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- Use Tree-sitter to locate and remove the node
+  local parser = vim.treesitter.get_parser(bufnr, 'norg')
+  local tree = parser:parse()[1]
+  local root = tree:root()
+
+  -- Define the Tree-sitter query
+  local query = vim.treesitter.query.parse('norg', [[
+        (heading1 title: (_) @title
+          (#eq? @title "Index"))
+        @whole
+    ]])
+
+  -- Find and delete the matched node
+  for id, node, _, _ in query:iter_captures(root, bufnr) do
+    local name = query.captures[id]
+
+    if (name == "whole") then
+      local start_row, _, end_row, _ = node:range()
+
+      vim.api.nvim_buf_set_lines(bufnr, start_row, end_row, true, {})
+    end
+  end
+
+  -- Locate the position after '@end' following '@document.meta'
+  local line_count = vim.api.nvim_buf_line_count(bufnr)
+  local found_end = false
+  for line = 1, line_count do
+    local line_text = vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
+    if found_end then
+      vim.api.nvim_buf_set_lines(bufnr, line - 1, line - 1, false, { "* Index" })
+      -- Set cursor to the newly inserted line
+      vim.api.nvim_win_set_cursor(0, { line, 0 })  -- Adjust cursor to the "* Index" line
+      -- Assuming you want to run a Neorg command on this line
+      vim.api.nvim_command('Neorg generate-workspace-summary')
+      break
+    end
+    if line_text:match("@end") then
+      found_end = true
+    end
+  end
+end
+
 M.config = function()
   require("neorg").setup {
     -- Tell Neorg what modules to load
@@ -38,6 +83,14 @@ M.config = function()
 
   vim.keymap.set(
     "n",
+    "<leader>ns",
+    updateIndexAndRunNeorg,
+    { noremap = true, silent = true }
+  )
+
+
+  vim.keymap.set(
+    "n",
     "<leader>nr",
     ":Neorg return<CR>",
     { noremap = true, silent = true }
@@ -52,6 +105,12 @@ M.config = function()
   vim.keymap.set(
     "n",
     "<leader>nt",
+    ":Neorg journal today<CR>",
+    { noremap = true, silent = true }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>ny",
     ":Neorg journal today<CR>",
     { noremap = true, silent = true }
   )
